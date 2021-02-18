650049be10cd5c2dc940a4fced62be135aa1fe47
Riza Masta <rmasta@pmintl.net>
2021-02-18 12:56:42 +0700

    improvement

--- src/app/pages/employee/overview.js
+++ src/app/pages/employee/overview.js
@@ -3,28 +3,39 @@ import { MainBar } from "app/components/app-bar";
 import { Button, Typography } from "@material-ui/core";
 import AppHistory from "app/utils";
 import { RequestGet } from "app/utils/HttpReq";
+let removeDuplicate = data => data.filter((v, i) => data.indexOf(v) === i);
 export default class EmployeOverview extends React.Component {
   state = { name: "", error: null, data: [] };
   componentDidMount() {
-    this.getEmployee();
-  }
-
-  getEmployee() {
     var params = new URLSearchParams(this.props.location.search);
+    this.setState({ name: params.get("name"), error: null });
     if (params.get("name")) {
-      this.setState({ name: params.get("name"), error: null });
-      RequestGet("assignment/employees/" + params.get("name"))
-        .then(r => {
-          r.data[1]
-            ? this.setState({ data: r.data[1]["direct-subordinates"] })
-            : this.setState({ error: "Doesn't have Subordinates" });
-        })
-        .catch(e => {
-          this.setState({ error: e.message });
-        });
+      this.getEmployee(params.get("name"));
     } else {
       AppHistory.push("404");
     }
+  }
+
+  getEmployee(n) {
+    RequestGet("assignment/employees/" + n)
+      .then(r => {
+        if (r.data[1]) {
+          var d = r.data[1]["direct-subordinates"].map(v => {
+            this.getEmployee(v);
+            return v;
+          });
+          this.setState({ data: [...this.state.data, ...d] }, () => {
+            this.setState({ data: removeDuplicate(this.state.data) });
+          });
+        } else {
+          if (n === this.state.name) {
+            this.setState({ error: "Doesn't have Subordinates" });
+          }
+        }
+      })
+      .catch(e => {
+        this.setState({ error: e.message });
+      });
   }
 
   render() {
