38eab4169f79e3275e417bdd7b8a4f4716a3c3ce
Riza Masta <rmasta@pmintl.net>
2021-02-22 20:53:24 +0700

    - fix usage “then” instead of “async”, “await” - remove unnecessary setState - fix unreadable name of variables - usage of statefull components

--- src/app/pages/employee/overview.js
+++ src/app/pages/employee/overview.js
@@ -2,40 +2,71 @@ import React from "react";
 import { MainBar } from "app/components/app-bar";
 import { Button, Typography } from "@material-ui/core";
 import AppHistory from "app/utils";
-import { RequestGet } from "app/utils/HttpReq";
-let removeDuplicate = data => data.filter((v, i) => data.indexOf(v) === i);
+import { getEmployeeData } from "./employee-api";
+
+//error handler
+function Error(props) {
+  return <Typography color="secondary">{props.text}</Typography>;
+}
+function DataDisplay(props) {
+  return (
+    <table>
+      <tbody>
+        {props.data.map((value, index) => {
+          return (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{value}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
 export default class EmployeOverview extends React.Component {
-  state = { name: "", error: null, data: [] };
+  constructor(props) {
+    super(props);
+    const params = new URLSearchParams(this.props.location.search);
+    this.state = { name: params.get("name"), error: null, data: [] };
+  }
+
   componentDidMount() {
-    var params = new URLSearchParams(this.props.location.search);
-    this.setState({ name: params.get("name"), error: null });
-    if (params.get("name")) {
-      this.getEmployee(params.get("name"));
+    const { name } = this.state;
+    if (name) {
+      this.getEmployee(name);
     } else {
       AppHistory.push("404");
     }
   }
 
-  getEmployee(n) {
-    RequestGet("assignment/employees/" + n)
-      .then(r => {
-        if (r.data[1]) {
-          var d = r.data[1]["direct-subordinates"].map(v => {
-            this.getEmployee(v);
-            return v;
-          });
-          this.setState({ data: [...this.state.data, ...d] }, () => {
-            this.setState({ data: removeDuplicate(this.state.data) });
-          });
-        } else {
-          if (n === this.state.name) {
-            this.setState({ error: "Doesn't have Subordinates" });
-          }
+  async getEmployee(nameValue) {
+    const { data: dataEmployee, error: message } = await getEmployeeData(
+      nameValue
+    );
+
+    const { data, name } = this.state;
+
+    if (message) {
+      this.setState({ error: message });
+    } else if (dataEmployee[1]) {
+      const newValue = await dataEmployee[1]["direct-subordinates"].map(
+        name => {
+          this.getEmployee(name);
+          return data.indexOf(name) < 0 ? name : null;
         }
-      })
-      .catch(e => {
-        this.setState({ error: e.message });
+      );
+      this.setState({
+        data: [
+          ...data,
+          ...newValue.filter(filteredValue => filteredValue != null),
+        ],
       });
+    } else {
+      if (nameValue === name) {
+        this.setState({ error: "Doesn't have Subordinates" });
+      }
+    }
   }
 
   render() {
@@ -45,19 +76,8 @@ export default class EmployeOverview extends React.Component {
         <MainBar title="Employee Overview" />
         <Typography>Employee Overview</Typography>
         <Typography>Subordinates of employee {name}:</Typography>
-        <table>
-          <tbody>
-            {data.map((v, i) => {
-              return (
-                <tr key={i}>
-                  <td>{i + 1}</td>
-                  <td>{v}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        {error && <Typography color="secondary">{error}</Typography>}
+        <DataDisplay data={data} />
+        <Error text={error} />
         <Button
           variant="contained"
           color="primary"
